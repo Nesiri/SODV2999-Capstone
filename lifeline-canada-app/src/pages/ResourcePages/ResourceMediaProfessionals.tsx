6031b694b28bf05b0748df53f090cf2b7028037b
Carlos1026-web <Derek.carlos04@gmail.com>
2026-04-12 01:40:03 -0600

    updated titles and links to lifeline purple for pages under resources

--- lifeline-canada-app/src/pages/ResourcePages/ResourceMediaProfessionals.tsx
+++ lifeline-canada-app/src/pages/ResourcePages/ResourceMediaProfessionals.tsx
@@ -2,7 +2,7 @@ import MentalHealthResourcesMediaProfessionals from "../../components/MentalHeal
 
 const ResourceMediaProfessionals = () => {
     return (
-        <div className="relative top-20 sm:top-0 min-h-screen bg-gray-50">
+        <div className="relative top-20 sm:top-0 min-h-screen bg-gray-50 resources-page-theme">
             <div className="w-full px-4 sm:px-6 lg:px-8 py-12">
                 <MentalHealthResourcesMediaProfessionals />
             </div>
--- lifeline-canada-app/src/pages/ResourcePages/ResourcePTSD.tsx
+++ lifeline-canada-app/src/pages/ResourcePages/ResourcePTSD.tsx
@@ -2,7 +2,7 @@ import MentalHealthResourcesPTSD from "../../components/MentalHealthResources/Re
 
 const ResourcePTSD = () => {
     return (
-        <div className="relative top-20 sm:top-0 min-h-screen bg-gray-50">
+        <div className="relative top-20 sm:top-0 min-h-screen bg-gray-50 resources-page-theme">
             <div className="w-full px-4 sm:px-6 lg:px-8 py-12">
                 <MentalHealthResourcesPTSD />
             </div>
